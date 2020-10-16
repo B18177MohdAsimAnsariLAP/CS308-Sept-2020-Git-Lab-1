1f42ba093ae44584a713975fbf7c2a83e3adb02f
Aljazri <b18177@students.iitmandi.ac.in>
2020-10-16 20:25:33 +0530

    added the prefix 'negsupport' to br_negativeSupport branch..

--- sqrt.c
+++ sqrt.c
@@ -8,18 +8,18 @@ positive number. */
 int main(int argc, char* argv[]) {
 	
 	if (argc != 2) {
-		printf("Usage: sqrt input\n\n");
+		printf("NEGSUPPORT: Usage: sqrt input\n\n");
 		exit(-1);
 	}
 
 	int input = atoi(argv[1]);
 	if(input<0){
 		//printf("invalid input!!\n\n");
-		printf("Sqrt of given no. %d is %fi\n",input,sqrt(-input));
+		printf("NEGSUPPORT: Sqrt of given no. %d is %fi\n",input,sqrt(-input));
 		exit(-1);
 	}
-	printf("Sqrt of %d is %f\n",input,sqrt(input));
-	printf("End of program. Exiting..\n\n");
+	printf("NEGSUPPORT: Sqrt of %d is %f\n",input,sqrt(input));
+	printf("NEGSUPPORT: End of program. Exiting..\n\n");
 	return(0);
 
 } // end main
